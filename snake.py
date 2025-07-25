a10ac50ad9662bfdf2662df4093099fbccf72338
sreyaascu <sreyaast@gmail.com>
2025-07-26 00:43:19 +0530

    Changing the movement logic of the snake

--- snake.py
+++ snake.py
@@ -13,11 +13,14 @@ clock = pg.time.Clock()
 x_pos = 300
 y_pos = 200
 
-snake = pg.Rect(x_pos,y_pos,50,50);         #x_pos and y_pos are the coordinates and 50 - 50 are the size
+block_size = 50
+
+snake_rect = pg.Rect(x_pos,y_pos,50,50);         #x_pos and y_pos are the coordinates and 50 - 50 are the size
 food_color = (170,70,30)
 snake_color = (70,120,210)
 global food
 food = None 
+
 def random_x():
     x_cord = rd.randrange(0,751,5)
     return x_cord
@@ -26,89 +29,72 @@ def random_y():
     y_cord = rd.randrange(0,551,5)
     return y_cord
 
-
+ini = 0
 left=right=up=down = False
+bg_color = (175,215,70)
+snake = [snake_rect]
+pg.draw.rect(screen,snake_color,snake[0])
+growing = False
 
-
-global temp_x 
-global temp_y 
-
-temp_x = None
-temp_y = None
-temp_y = None
 while playing:
+    screen.fill(bg_color)
+    if ini==0:
+        pg.draw.rect(screen,snake_color,snake[0])
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             playing = False
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_UP:
-                up = True
-                down=right=left = False
-            elif event.key == pg.K_DOWN:
+
+            if event.key == pg.K_DOWN:
                 down = True
-                up=left=right = False
-            elif event.key == pg.K_LEFT:
-                left = True
-                down=up=right = False
+                left = right = up = False
+            elif event.key == pg.K_UP:
+                up = True
+                down = right = left = False
             elif event.key == pg.K_RIGHT:
                 right = True
-                left=down=up = False
+                left = up = down = False
+            elif event.key == pg.K_LEFT:
+                left = True
+                right = up = down = False
 
-#Screen dimensions : (800,600)
-            
-    screen.fill((175,215,70))
-        
+
+
+    
+    new_head = snake[0].copy()
+    snake.insert(0,new_head)
     if up:
-        snake.y-= 5
-        if snake.y > 600 or snake.y < 0:
-            snake.y = 200
-            snake.x = 300
-            up = False        
+            
+        if not growing:
+           snake.pop()
+           for i in snake:
+               i.y-=block_size
+               pg.draw.rect(screen,snake_color,i)
+        else:
+            for i in snake:
+                i.y-=block_size
+                pg.draw.rect(screen,snake_color,i)
+
     elif down:
-        snake.y += 5
-
-        if snake.y > 550 or snake.y < 0:
-            snake.y = 200
-            snake.x = 300
-            down = False
-    elif right:
-        snake.x += 5
-
-        if snake.x > 750 or snake.x < 0:
-            snake.y = 200
-            snake.x = 300
-            right = False
-    elif left:
-        snake.x -= 5
-
-        if snake.x > 800 or snake.x < 0:
-            snake.y = 200
-            snake.x = 300
-            left = False
+        if not growing:
+            snake.pop()
+            for i in snake:
+                i.y+=block_size
+                pg.draw.rect(screen,snake_color,i)
+        else:
+            for i in snake:
+                i.y+=block_size
+                pg.draw.rect(screen,snake_color,i)
 
 
-
-    #Snake
-    pg.draw.rect(screen,snake_color,snake)
-    ini_x = 500
-    ini_y = 100
-    if(ct==0):
-        food = pg.Rect(ini_x,ini_y,50,50)
-        pg.draw.rect(screen,food_color,food)
-        ct=1
-
-    if(snake.colliderect(food)):
-        screen.fill((175,215,70))
-        temp_x = random_x()
-        temp_y = random_y()
-        food = pg.Rect(temp_x,temp_y,50,50)
-        pg.draw.rect(screen,snake_color,snake)
-    else:
-        pg.draw.rect(screen,food_color,food)
-
-
+    
     pg.display.update()
+    clock.tick(6)
 
-   # test_surface.fill((70,120,210))
-    clock.tick(60) # loop runs max 60 times in a sec 
+
+
+
+
+
 
